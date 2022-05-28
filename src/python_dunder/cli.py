46e6641bf98785d5e62b8af2b2cd4c1032a587d7
Tateishi Tadatoshi <ishio39@gmail.com>
2022-05-28 15:07:41 +0900

    __new__(), __repr__() のサンプル

--- src/python_dunder/cli.py
+++ src/python_dunder/cli.py
@@ -1,2 +1,27 @@
+from fire import Fire
+
+from . import core
+
+
 def hello():
     print("Hello, World.")
+
+
+def new_test_main():
+    a = core.TestNew(1)
+    assert a.a == 1
+
+
+def new_test():
+    Fire(new_test_main)
+
+
+def repr_test_main():
+    a1 = core.ReprTest1()
+    a2 = core.ReprTest2()
+    print(f"a1={a1}, a2={a2}")
+    print(f"a1={repr(a1)}, a2={repr(a2)}")
+
+
+def repr_test():
+    Fire(repr_test_main)
--- src/python_dunder/core.py
+++ src/python_dunder/core.py
@@ -1,2 +1,20 @@
 def foo():
     return "foo"
+
+
+class TestNew:
+    def __new__(cls, *args, **kwarg):
+        instance = super().__new__(cls)
+        print(f"__new__(cls) called with class {cls} and arguments {args}, {kwarg}")
+        instance.a = args[0]
+
+        return instance
+
+
+class ReprTest1:
+    pass
+
+
+class ReprTest2:
+    def __repr__(self):
+        return f"repr_test2({id(self):x})"
